229f5e011172bdea90e44529b7ab520b3ccf2c49
jwsong98 <thdwldnd7@naver.com>
2025-06-07 03:50:09 +0900

    feat: 페스티벌의 세부 정보를 전부 조회 API 추가
    related-to: #4

---
+++ src/main/java/ddd/darayo/festival/presentation/performance/PerformanceController.java
@@ -0,0 +1,23 @@
+package ddd.darayo.festival.presentation.performance;
+
+import ddd.darayo.festival.domain.service.PerformanceManagement;
+import ddd.darayo.festival.presentation.performance.exchanges.PerformanceDetailRes;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/performance")
+@RequiredArgsConstructor
+public class PerformanceController {
+    private final PerformanceManagement performanceManagement;
+
+    @GetMapping("/all/detail")
+    public ResponseEntity<List<PerformanceDetailRes>> getAllPerformanceDetails() {
+        return ResponseEntity.ok(performanceManagement.findAllDetail());
+    }
+}
