f103b20fc782f8128621d9944efafb1185dd8a30
jwsong98 <thdwldnd7@naver.com>
2025-06-06 23:52:08 +0900

    feat: 관리자 인증 로직 추가
    related-to: #4

--- src/main/java/ddd/darayo/festival/presentation/performance/PerformanceAdminController.java
+++ src/main/java/ddd/darayo/festival/presentation/performance/PerformanceAdminController.java
@@ -1,9 +1,11 @@
 package ddd.darayo.festival.presentation.performance;
 
 import ddd.darayo.festival.domain.entity.Performance;
+import ddd.darayo.festival.domain.service.AuthService;
 import ddd.darayo.festival.domain.service.PerformanceManagement;
 import ddd.darayo.festival.presentation.performance.exchanges.SavePerformanceReq;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,8 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class PerformanceAdminController {
     private final PerformanceManagement performanceManagement;
 
+    private final AuthService authService;
+
     @PostMapping
     public ResponseEntity<Long> createPerformance(@RequestBody SavePerformanceReq req) {
+        if (!authService.authenticate(req.getPassword())) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
         Performance performance = performanceManagement.save(req);
         return ResponseEntity.ok(performance.getId());
     }
