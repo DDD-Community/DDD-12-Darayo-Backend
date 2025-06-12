4e6a666ba6365d26659027c4381703d896a0abbf
jwsong98 <thdwldnd7@naver.com>
2025-06-12 21:58:33 +0900

    fix: 아티스트 별명 수정 메소드에 누락된 Modifying 어노테이션 추가
    related-to: #4

--- src/main/java/ddd/darayo/festival/domain/repository/ArtistAliasRepository.java
+++ src/main/java/ddd/darayo/festival/domain/repository/ArtistAliasRepository.java
@@ -2,6 +2,7 @@ package ddd.darayo.festival.domain.repository;
 
 import ddd.darayo.festival.domain.entity.ArtistAlias;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ArtistAliasRepository extends JpaRepository<ArtistAlias, Long> {
     @Query("UPDATE ArtistAlias al SET al.name = :alias WHERE al.id = :aliasId")
+    @Modifying
     int updateArtistAlias(
             @Param("alias") String alias,
             @Param("aliasId") Long aliasId
