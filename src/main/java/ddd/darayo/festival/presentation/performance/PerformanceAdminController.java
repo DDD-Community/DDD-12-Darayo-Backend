0dc425c05076444c79c92bffe154dfea8a3c57b1
jwsong98 <thdwldnd7@naver.com>
2025-06-13 20:57:56 +0900

    feat: 시간표 생성 API 추가
    related-to: #4

--- src/main/java/ddd/darayo/festival/presentation/performance/PerformanceAdminController.java
+++ src/main/java/ddd/darayo/festival/presentation/performance/PerformanceAdminController.java
@@ -1,8 +1,11 @@
 package ddd.darayo.festival.presentation.performance;
 
 import ddd.darayo.festival.domain.entity.Performance;
+import ddd.darayo.festival.domain.entity.Timetable;
 import ddd.darayo.festival.domain.service.AuthService;
 import ddd.darayo.festival.domain.service.PerformanceManagement;
+import ddd.darayo.festival.domain.service.TimetableManagement;
+import ddd.darayo.festival.presentation.performance.exchanges.AddTimetableReq;
 import ddd.darayo.festival.presentation.performance.exchanges.PerformanceDetailRes;
 import ddd.darayo.festival.presentation.performance.exchanges.SavePerformanceReq;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +22,7 @@ public class PerformanceAdminController {
     private final PerformanceManagement performanceManagement;
 
     private final AuthService authService;
+    private final TimetableManagement timetableManagement;
 
     @PostMapping
     public ResponseEntity<Long> createPerformance(@RequestBody SavePerformanceReq req) {
@@ -38,5 +42,14 @@ public class PerformanceAdminController {
     public ResponseEntity<Void> deletePerformance(@PathVariable Long performanceId) {
         performanceManagement.delete(performanceId);
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/{performanceId}/timetable")
+    public ResponseEntity<Long> createTimetable(
+            @PathVariable Long performanceId,
+            @RequestBody AddTimetableReq req
+    ) {
+        Timetable timetable = timetableManagement.addTimetable(performanceId, req);
+        return ResponseEntity.ok(timetable.getId());
     }
 }
