d2622eccaf6c8bf4525ec0c036100a3b4b330f32
jwsong98 <thdwldnd7@naver.com>
2025-06-14 11:00:24 +0900

    feat: 시간표 수정 API 추가
    related-to: #4

---
+++ src/main/java/ddd/darayo/festival/presentation/timetable/TimetableAdminController.java
@@ -0,0 +1,25 @@
+package ddd.darayo.festival.presentation.timetable;
+
+import ddd.darayo.festival.domain.service.TimetableManagement;
+import ddd.darayo.festival.presentation.timetable.exchanges.EditTimetableReq;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/admin/timetable")
+@RequiredArgsConstructor
+public class TimetableAdminController {
+    private final TimetableManagement timetableManagement;
+
+    @PutMapping("/{timetableId}")
+    public ResponseEntity<Void> editTimetable(
+            @PathVariable("timetableId") long timetableId,
+            @RequestBody EditTimetableReq req
+    ) {
+        timetableManagement.editTimetable(timetableId, req);
+        return ResponseEntity.ok().build();
+    }
+
+
+}
