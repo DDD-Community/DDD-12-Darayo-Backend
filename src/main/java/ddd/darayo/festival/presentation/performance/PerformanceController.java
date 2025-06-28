9d9c943dabdc238054d3b341fe02a715737ec284
jwsong98 <thdwldnd7@naver.com>
2025-06-28 14:57:25 +0900

    feat: 사용자 페스티벌 시간표 조회 API 구현
    - 페스티벌 ID를 통해 시간표를 조회할 수 있는 새로운 API 엔드포인트 추가
    - 시간표 엔티티를 사용자 응답 형식으로 변환하는 비즈니스 로직 및 매퍼 정의
    
    related-to: #8

--- src/main/java/ddd/darayo/festival/presentation/performance/PerformanceController.java
+++ src/main/java/ddd/darayo/festival/presentation/performance/PerformanceController.java
@@ -1,10 +1,13 @@
 package ddd.darayo.festival.presentation.performance;
 
 import ddd.darayo.festival.domain.service.PerformanceManagement;
+import ddd.darayo.festival.domain.service.TimetableManagement;
 import ddd.darayo.festival.presentation.performance.exchanges.UserGetPerformanceInfo;
+import ddd.darayo.festival.presentation.performance.exchanges.UserGetTimetableRes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +18,17 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PerformanceController {
     private final PerformanceManagement performanceManagement;
+    private final TimetableManagement timetableManagement;
 
     @GetMapping
     public ResponseEntity<List<UserGetPerformanceInfo>> getPerformances() {
         return ResponseEntity.ok(performanceManagement.findUserPerformance());
+    }
+
+    @GetMapping("/{festivalId}/timetable")
+    public ResponseEntity<List<UserGetTimetableRes>> getPerformancesForFestival(
+            @PathVariable Long festivalId
+    ) {
+        return ResponseEntity.ok(timetableManagement.getUserGetTimetables(festivalId));
     }
 }
