08474e9cc25789a9f504bc1014030e46c591c9a2
jwsong98 <thdwldnd7@naver.com>
2025-07-05 01:41:16 +0900

    [FEAT] : 예매 시작 날짜가 특정 날짜 사이인 공연 정보 조회 로직 구현
    related-to: #11

--- src/main/java/ddd/darayo/festival/domain/repository/PerformanceRepository.java
+++ src/main/java/ddd/darayo/festival/domain/repository/PerformanceRepository.java
@@ -3,8 +3,10 @@ package ddd.darayo.festival.domain.repository;
 import ddd.darayo.festival.domain.entity.Performance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -19,4 +21,14 @@ public interface PerformanceRepository extends JpaRepository<Performance, Long> 
     """)
     List<Performance> findAllDetail();
 
+    @Query("""
+        SELECT DISTINCT p
+            FROM Performance p
+            JOIN FETCH p.reservationInfos r
+        WHERE r.openDateTime BETWEEN :start AND :end
+    """)
+    List<Performance> findByReservationOpenDateBetween(
+        @Param("start") LocalDateTime start,
+        @Param("end") LocalDateTime end
+    );
 }
