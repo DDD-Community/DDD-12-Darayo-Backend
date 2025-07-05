a6b47203b27fd72df14e713c4641efc79e8a8d27
jwsong98 <thdwldnd7@naver.com>
2025-07-05 14:20:21 +0900

    [FIX]: Payload 필드 추가
    Related-to: #11

--- src/main/java/ddd/darayo/festival/domain/service/AlarmMessageFormatter.java
+++ src/main/java/ddd/darayo/festival/domain/service/AlarmMessageFormatter.java
@@ -3,6 +3,8 @@ package ddd.darayo.festival.domain.service;
 import ddd.darayo.festival.domain.entity.Performance;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 public class AlarmMessageFormatter {
     private static final String RESERVATION_DUE_ALARM_TITLE_FORMAT = "\"%s\" 예매 알림";
@@ -14,26 +16,33 @@ public class AlarmMessageFormatter {
 
     private static final String CLICK_DESCRIPTION = "눌러서 자세한 내용을 확인해보세요.";
 
-    public record MessageContent(String title, String description) {}
+    public record MessageContent(
+        String title,
+        String description,
+        Map<String, String> data
+    ) {}
 
     public MessageContent reservationDueDateAlarm(Performance p, int day) {
         return new MessageContent(
                 String.format(RESERVATION_DUE_ALARM_TITLE_FORMAT, p.getName()),
-                String.format(RESERVATION_DUE_ALARM_DESCRIPTION_FORMAT, day)
+                String.format(RESERVATION_DUE_ALARM_DESCRIPTION_FORMAT, day),
+                Map.of()
         );
     }
 
     public MessageContent performanceGuideAlarm(Performance p) {
         return new MessageContent(
                 String.format(PERFORMANCE_GUIDE_ALARM_TITLE_FORMAT, p.getName()),
-                CLICK_DESCRIPTION
+                CLICK_DESCRIPTION,
+                Map.of()
         );
     }
 
     public MessageContent performanceTimetableAlarm(Performance p) {
         return new MessageContent(
                 String.format(PERFORMANCE_TIMETABLE_ALARM_TITLE_FORMAT, p.getName()),
-                CLICK_DESCRIPTION
+                CLICK_DESCRIPTION,
+                Map.of()
         );
     }
 }
