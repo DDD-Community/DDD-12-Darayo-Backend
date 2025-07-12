096eebb70e400cc8260dc412e67ba76eb03abd2b
jwsong98 <thdwldnd7@naver.com>
2025-07-12 15:10:10 +0900

    [FEAT]: 유저 공연 알림 설정 및 설정 해제 메세드 추가
    Related-to: #13
    
    # Conflicts:
    #	src/main/java/ddd/darayo/festival/domain/exception/constant/AlarmError.java

--- src/main/java/ddd/darayo/festival/domain/exception/constant/AlarmError.java
+++ src/main/java/ddd/darayo/festival/domain/exception/constant/AlarmError.java
@@ -7,6 +7,8 @@ import lombok.AllArgsConstructor;
 public enum AlarmError implements ErrorInfo {
     ALARM_SEND_FAILED("5000", "알림 전송에 실패했습니다."),
     ALARM_INVALID_TOKEN("5001", "유효하지 않은 알림 토큰입니다."),
+    ALARM_ALREADY_FESTIVAL("5002", "이미 알림을 등록한 페스티벌입니다."),
+    ALARM_NOT_EXIST_FESTIVAL("5003", "알림 등록되지 않은 페스티벌입니다.")
     ;
 
     private final String code;
@@ -21,4 +23,4 @@ public enum AlarmError implements ErrorInfo {
     public String getCode() {
         return code;
     }
-} 
+}
