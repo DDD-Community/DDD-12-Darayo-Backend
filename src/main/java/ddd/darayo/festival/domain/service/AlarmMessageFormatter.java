446428ff433712140fe5bc1a62328886daa6ed8c
jwsong98 <thdwldnd7@naver.com>
2025-07-12 11:49:59 +0900

    [FIX]: 알림 멘트 요구사항 반영
    related-to: #11

--- src/main/java/ddd/darayo/festival/domain/service/AlarmMessageFormatter.java
+++ src/main/java/ddd/darayo/festival/domain/service/AlarmMessageFormatter.java
@@ -7,12 +7,12 @@ import java.util.Map;
 
 @Service
 public class AlarmMessageFormatter {
-    private static final String RESERVATION_DUE_ALARM_TITLE_FORMAT = "\"%s\" 예매 알림";
-    private static final String RESERVATION_DUE_ALARM_DESCRIPTION_FORMAT = "%d일 후 예매가 예정되어 있습니다.";
+    private static final String RESERVATION_DUE_ALARM_TITLE_FORMAT = "%s 티켓 예매가 %s 잊지말고 준비하세요 \uD83D\uDD25";
+    private static final String DAY_MENTION_FORMAT = "%d일 남았어요!";
+    private static final String TODAY_MENTION_FORMAT = "오늘이에요!";
 
-    private static final String PERFORMANCE_GUIDE_ALARM_TITLE_FORMAT = "\"%s\" 반입 물품 및 교통 안내 알림";
-
-    private static final String PERFORMANCE_TIMETABLE_ALARM_TITLE_FORMAT=  "\"%s\" 시간표 안내 알림";
+    private static final String PERFORMANCE_GUIDE_ALARM_TITLE_FORMAT = "%s이(가) 내일이에요! 반입물품과 교통편을 미리 확인하세요 \uD83D\uDCE2";
+    private static final String PERFORMANCE_TIMETABLE_ALARM_TITLE_FORMAT=  "%s이(가) 3일 남았어요! 타임테이블을 미리 확인해보세요 \uD83D\uDCC6";
 
     private static final String CLICK_DESCRIPTION = "눌러서 자세한 내용을 확인해보세요.";
 
@@ -23,9 +23,14 @@ public class AlarmMessageFormatter {
     ) {}
 
     public MessageContent reservationDueDateAlarm(Performance p, int day) {
+        String dayMention = switch(day) {
+            case 0 -> TODAY_MENTION_FORMAT;
+            default -> String.format(DAY_MENTION_FORMAT, day);
+        };
+
         return new MessageContent(
-                String.format(RESERVATION_DUE_ALARM_TITLE_FORMAT, p.getName()),
-                String.format(RESERVATION_DUE_ALARM_DESCRIPTION_FORMAT, day),
+                String.format(RESERVATION_DUE_ALARM_TITLE_FORMAT, p.getName(), dayMention),
+                CLICK_DESCRIPTION,
                 Map.of()
         );
     }
