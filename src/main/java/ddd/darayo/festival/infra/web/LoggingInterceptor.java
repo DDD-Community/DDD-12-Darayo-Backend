db213ec319935c13880c360c736c996de1a28d6f
jwsong98 <thdwldnd7@naver.com>
2025-07-30 00:45:55 +0900

    [feat]: START/END 로그 추가

--- src/main/java/ddd/darayo/festival/infra/web/LoggingInterceptor.java
+++ src/main/java/ddd/darayo/festival/infra/web/LoggingInterceptor.java
@@ -3,13 +3,10 @@ package ddd.darayo.festival.infra.web;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.util.ContentCachingRequestWrapper;
-import org.springframework.web.util.ContentCachingResponseWrapper;
 
-import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 @Slf4j
@@ -25,24 +22,23 @@ public class LoggingInterceptor implements HandlerInterceptor {
         log.info(body);
 
         String requestId = UUID.randomUUID().toString();
-        HttpServletRequest finalRequest = request;
         StringBuilder headerBuilder = new StringBuilder();
         if (request.getHeaderNames().hasMoreElements()) {
             headerBuilder.append("{\n");
             request.getHeaderNames().asIterator().forEachRemaining(h -> {
                 headerBuilder.append("\t").append(h);
                 headerBuilder.append(": ");
-                headerBuilder.append(finalRequest.getHeader(h));
+                headerBuilder.append(request.getHeader(h));
                 headerBuilder.append("\n");
             });
             headerBuilder.append("}");
         }
 
-        log.info("\n[{}] {} {} {}\n" +
+        log.info("\n[{}] START {} {} {}\n" +
                 "Headers: {}\n" +
                 "Request Body: {}\n",
                 requestId, request.getMethod(), request.getRequestURI(), request.getQueryString(),
-                headerBuilder.toString(),
+                headerBuilder,
                 body
         );
 
@@ -57,6 +53,6 @@ public class LoggingInterceptor implements HandlerInterceptor {
         String requestId = (String) request.getAttribute("requestId");
         long elapsedMs = (System.nanoTime() - (long) request.getAttribute("startNanos")) / 1_000_000;
 
-        log.info("[{}] Duration: {} ms Status: {}", requestId, elapsedMs, response.getStatus());
+        log.info("[{}] END Duration: {} ms Status: {}", requestId, elapsedMs, response.getStatus());
     }
 }
